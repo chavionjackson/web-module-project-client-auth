376d9daeceb78c1ee05ee1ed121ec4382ccb1e41
Chevy <75184977+chavionjackson@users.noreply.github.com>
2021-09-05 10:39:31 -0700

    set up friends list

--- friends/src/components/FriendList.js
+++ friends/src/components/FriendList.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import React from "react";
+import { Card, CardText, CardBody, CardTitle, CardSubtitle } from "reactstrap";
 
 class FriendsList extends React.Component {
   state = {
@@ -11,12 +12,36 @@ class FriendsList extends React.Component {
 
   getData = () => {
     axios
-      .get("http://localhost:5000/api/friends", { headers: { Authorization: localStorage.getItem('token') } })
-      .then((res) => console.log(res))
+      .get("http://localhost:5000/api/friends", {
+        headers: { Authorization: localStorage.getItem("token") },
+      })
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          friendsList: res.data,
+        });
+      })
       .catch((err) => console.log("unable to get data", { err }));
   };
   render() {
-    return <h1>Friends List</h1>;
+    return (
+      <div className="card-container">
+        <h1>My Friends!</h1>
+        {this.state.friendsList.map((friend) => (
+          <Card key={friend.id} style={{ width: "18rem" }}>
+            <CardBody>
+              <CardTitle>{friend.name}</CardTitle>
+              <CardSubtitle className="mb-2 text-muted">
+                {friend.age}
+              </CardSubtitle>
+              <CardText>
+                {friend.email}
+              </CardText>
+            </CardBody>
+          </Card>
+        ))}
+      </div>
+    );
   }
 }
 
